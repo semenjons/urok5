ba5d51003ad1515e43b247700bfb9e5c5a95eb41
semen.jons <semen.jons@yandex.ru>
2022-11-27 14:48:08 +0300

    Количетво четных чисел в массиве

---
+++ threenumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿// : Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+int [] array = MassArrayRandomNumbers(numbers);
+Console.WriteLine($"{string.Join(" ", array)} ");
+
+int count = 0;
+MassArrayRandomNumbers(numbers);
+for (int z = 0; z < numbers.Length; z++)
+if (numbers[z] % 2 == 0)
+count++;
+
+Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+
+int [] MassArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,999);
+    }
+    return numbers;
+}
+
+
